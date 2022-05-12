f16d8ae592944dc913ce60a881ced547b9507093
Jeongsu Lee <47911773+sw930718@users.noreply.github.com>
2022-05-12 16:38:47 +0900

    [Update] custom_runtime.py

--- mmseg/Beit/Configs/_base_/custom_runtime.py
+++ mmseg/Beit/Configs/_base_/custom_runtime.py
@@ -15,7 +15,7 @@ dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
 resume_from = None
-workflow = [('train', 1),('val',1)]
+workflow = [('train', 1),('val',1)] # wandb train, val 확인 코드  
 cudnn_benchmark = True
 
 runner = dict(type='EpochBasedRunner', max_epochs=40)
